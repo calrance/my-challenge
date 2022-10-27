946e2c76395546c101ca148bde9e59188e7ff371
Calrance Kho <calrancedev@gmail.com>
2022-10-27 15:05:28 +0800

    project setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+
+    <div class="container">
+      <div class="mt-5">
+        <h1>Rates List</h1>
+
+        <div class="flex">
+          hello
+        </div>
+      </div>
+    </div>
+
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,126 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: '#app',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      'sm': 'var(--text-sm)',
+      'base': 'var(--text-base)',
+      'md': 'var(--text-md)',
+      'lg': 'var(--text-lg)',
+      'xl': 'var(--text-xl)',
+      '2xl': 'var(--text-2xl)',
+      'display-1': 'var(--text-display-1)',
+      'display-2': 'var(--text-display-2)',
+    },
+    colors: {
+      'primary': 'var(--color-primary)',
+      'secondary': 'var(--color-secondary)',
+      'dark-blue': 'var(--color-dark-blue)',
+      'light-blue': 'var(--color-light-blue)',
+
+      'body-text': 'var(--color-body-text)',
+      'heading-text': 'var(--color-heading-text)',
+      'heading-text-2': 'var(--color-heading-text-2)',
+
+      'black': 'var(--color-black)',
+      'black-2': 'var(--color-black-2)',
+      'gray': 'var(--color-gray)',
+      'gray-2': 'var(--color-gray-2)',
+      'white': 'var(--color-white)',
+
+      'placeholder': 'var(--color-placeholder)',
+      'border': 'var(--color-border)',
+
+      'success': 'var(--color-success)',
+      'warning': 'var(--color-warning)',
+      'danger': 'var(--color-danger)',
+    },
+    borderRadius: {
+      xl: '1rem'
+    },
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1400px',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0.75rem',
+      },
+      screens: {
+        sm: '540px',
+        md: '720px',
+        lg: '960px',
+        xl: '1140px',
+        '2xl': '1320px',
+      },
+    },
+    extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+        '14': 'repeat(14, minmax(0, 1fr))',
+        '15': 'repeat(15, minmax(0, 1fr))',
+        '16': 'repeat(16, minmax(0, 1fr))',
+        '17': 'repeat(17, minmax(0, 1fr))',
+        '18': 'repeat(18, minmax(0, 1fr))',
+        '19': 'repeat(19, minmax(0, 1fr))',
+        '20': 'repeat(20, minmax(0, 1fr))',
+        '21': 'repeat(21, minmax(0, 1fr))',
+        '22': 'repeat(22, minmax(0, 1fr))',
+        '23': 'repeat(23, minmax(0, 1fr))',
+        '24': 'repeat(24, minmax(0, 1fr))',
+      },
+      gridColumn: {
+        'span-13': 'span 13 / span 13',
+        'span-14': 'span 14 / span 14',
+        'span-15': 'span 15 / span 15',
+        'span-16': 'span 16 / span 16',
+        'span-17': 'span 17 / span 17',
+        'span-18': 'span 18 / span 18',
+        'span-19': 'span 19 / span 19',
+        'span-20': 'span 20 / span 20',
+        'span-21': 'span 21 / span 21',
+        'span-22': 'span 22 / span 22',
+        'span-23': 'span 23 / span 23',
+        'span-24': 'span 24 / span 24',
+      },
+      gridColumnStart: {
+        '13': '13',
+        '14': '14',
+        '15': '15',
+        '16': '16',
+        '17': '17',
+        '18': '18',
+        '19': '19',
+        '20': '20',
+        '21': '21',
+        '22': '22',
+        '23': '23',
+        '24': '24',
+      },
+      gridColumnEnd: {
+        '13': '13',
+        '14': '14',
+        '15': '15',
+        '16': '16',
+        '17': '17',
+        '18': '18',
+        '19': '19',
+        '20': '20',
+        '21': '21',
+        '22': '22',
+        '23': '23',
+        '24': '24',
+        '25': '25',
+      }
+    }
+  },
+  plugins: [],
+}
