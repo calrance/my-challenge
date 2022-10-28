9bed74a9ae71817ac7602d68b3f17b09bbebdee5
Calrance Kho <calrancedev@gmail.com>
2022-10-28 11:52:16 +0800

    functionality update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { computed, reactive, ref } from '@vue/reactivity';
+import axios from 'axios';
+import { onMounted } from 'vue';
+
+let ratesListTable = ref();
+
+let page = ref(1);
+let pageSize = ref(5);
+let totalPages = ref(0);
+
+
+async function displayRatesList() {
+  const res = await axios.get("https://api.coingecko.com/api/v3/exchange_rates");
+
+  ratesListTable.value = Object.entries(res.data.rates).map((item: any[]) => ({
+    key: item[0],
+    info: item[1],
+  }));
+
+  // console.log(JSON.stringify(ratesListTable.value));
+  // console.log(JSON.stringify(res));
+}
+
+const displayRatesListTable = computed(() => {
+  return ratesListTable.value.slice(pageSize.value * page.value - pageSize.value, pageSize.value * page.value);
+});
+
+const handlePaginationChange = (val: number) => {
+  page.value = val;
+}
+
+const handlePageSizeChange = (val: number) => {
+  pageSize.value = val;
+}
+
+onMounted(() => {
+  displayRatesList();
+});
+
+
 </script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
-
     <div class="container">
       <div class="mt-5">
-        <h1>Rates List</h1>
+        <h1 class="mb-5 font-bold">Rates List</h1>
 
-        <div class="flex">
-          hello
+        <div v-if="ratesListTable">
+          <el-table :class="'mb-10'" :data="displayRatesListTable" style="width: 100%">
+            <el-table-column sortable prop="info.name" label="Name" />
+            <el-table-column sortable prop="info.type" label="Type" />
+            <el-table-column prop="info.unit" label="Unit" />
+            <el-table-column sortable prop="info.value" label="Value" />
+          </el-table>
+
+          <div class="text-right">
+            <el-pagination layout="sizes, prev, pager, next" :total="ratesListTable.length"
+              @current-change="handlePaginationChange" @size-change="handlePageSizeChange" :page-sizes="[5, 10, 25]"
+              :page-size="pageSize" />
+          </div>
         </div>
+
       </div>
     </div>
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,37 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(), 
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver({
+        importStyle: 'sass',
+      })],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver({
+        importStyle: 'sass',
+      })],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    // preprocessorOptions: {
+    //   scss: {
+    //     additionalData: `@use "@/assets/styles/element-plus/variables" as el;`,
+    //   },
+    // },
+    devSourcemap: true,
+  },
 })
